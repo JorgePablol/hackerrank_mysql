c7c55a3060c0f2a261c1538386f20de1d841c026
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-11 12:31:48 -0500

    Create select_by_id.sql

---
+++ select_by_id.sql
@@ -0,0 +1,8 @@
+/* 
+SELECT BY ID:
+  Query all columns for a city in CITY with the ID 1661.
+
+  The CITY table is described as follows(https://www.hackerrank.com/challenges/select-by-id/problem):
+*/
+
+SELECT * FROM CITY WHERE ID=1661;
