fb6c58dd2e7e4b9cea040b82f9755f1ce962872c
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-11 12:38:52 -0500

    Create weather_observation_station_1.sql

---
+++ weather_observation_station_1.sql
@@ -0,0 +1,7 @@
+/*
+WEATHER OBSERVATION STATION 1:
+Query a list of CITY and STATE from the STATION table.
+The STATION table is described as follows(https://www.hackerrank.com/challenges/weather-observation-station-1/problem):
+*/
+
+SELECT CITY, STATE FROM STATION;
