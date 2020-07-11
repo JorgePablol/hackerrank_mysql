7f1c9bf309c3c63b982203c727ad6138bbbb1027
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-11 12:30:16 -0500

    Create select_all.sql

---
+++ select_all.sql
@@ -0,0 +1,7 @@
+/* SELECT ALL:
+  Query all columns (attributes) for every row in the CITY table.
+
+   The CITY table is described as follows (https://www.hackerrank.com/challenges/select-all-sql/problem):
+*/
+
+SELECT * FROM CITY;
