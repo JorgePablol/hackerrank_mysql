d53132c59104651c128660d620a9a2315a80f4e7
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-14 19:59:46 -0500

    Update revising_aggregations_averages.sql

--- revising_aggregations_averages.sql
+++ revising_aggregations_averages.sql
@@ -1,6 +1,8 @@
 /*
-Query the average population of all cities in CITY where District is California.
-The CITY table is described as follows(https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem)
+REVISING AGGREGATIONS AVERAGES:
+
+  Query the average population of all cities in CITY where District is California.
+  The CITY table is described as follows(https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem)
 */
 
 SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
