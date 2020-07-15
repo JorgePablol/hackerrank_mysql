a86a258227715a6ed3828e91a51cc924a9e1612c
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-14 19:55:35 -0500

    Create revising_aggregations_sum_function.sql

---
+++ revising_aggregations_sum_function.sql
@@ -0,0 +1,6 @@
+/*
+Query the total population of all cities in CITY where District is California.
+The CITY table is described as follows(https://www.hackerrank.com/challenges/revising-aggregations-sum/problem)
+*/
+
+SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California';
