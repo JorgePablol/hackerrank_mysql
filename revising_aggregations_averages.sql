63cb92a4c5a0a29374dd528187b68d1d8b7b3e59
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-14 19:56:58 -0500

    Create revising_aggregations_averages.sql

---
+++ revising_aggregations_averages.sql
@@ -0,0 +1,6 @@
+/*
+Query the average population of all cities in CITY where District is California.
+The CITY table is described as follows(https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem)
+*/
+
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
