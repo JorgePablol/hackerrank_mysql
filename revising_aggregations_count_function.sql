0b420df78535d24d15ec8e7aab966b99346b951d
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-14 19:53:56 -0500

    Create revising_aggregations_count_function.sql

---
+++ revising_aggregations_count_function.sql
@@ -0,0 +1,6 @@
+/*
+Query a count of the number of cities in CITY having a Population larger than 100000.
+The CITY table is described as follows(https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem)
+*/
+
+SELECT COUNT(NAME) FROM CITY WHERE POPULATION > 100000;
