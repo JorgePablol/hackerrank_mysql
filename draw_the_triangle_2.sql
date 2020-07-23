9fa1639c5b12eeebc2f373ce9b774e23317a1532
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-23 18:23:09 -0500

    Update and rename draw_the_ttriangle_2.sql to draw_the_triangle_2.sql

--- draw_the_triangle_2.sql
+++ draw_the_triangle_2.sql
@@ -1,7 +1,7 @@
 /*
 DRAW THE TRIANGLE 2:
     P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
-
+    Link = https://www.hackerrank.com/challenges/draw-the-triangle-2/problem
 */
 
 SET @number = 0;
