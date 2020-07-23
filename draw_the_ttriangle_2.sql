f6bfea21d8416c443de7c99baf779c1f66887dee
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-07-23 18:22:34 -0500

    Create draw_the_ttriangle_2.sql

---
+++ draw_the_ttriangle_2.sql
@@ -0,0 +1,8 @@
+/*
+DRAW THE TRIANGLE 2:
+    P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+*/
+
+SET @number = 0;
+SELECT REPEAT('* ', @number := @number + 1) FROM information_schema.tables LIMIT 20;
