234f001630a7262c4f392d8d14a90d4850794050
Jorge Pablo <58957744+JorgePablol@users.noreply.github.com>
2020-09-28 20:29:03 -0500

    Update contest_leaderboard.sql

--- contest_leaderboard.sql
+++ contest_leaderboard.sql
@@ -8,7 +8,8 @@ LINK = https://www.hackerrank.com/challenges/contest-leaderboard/problem?
 
 */
 
-SELECT h.hacker_id, h.name, SUM(sscore) FROM Hackers AS h INNER JOIN (SELECT s.hacker_id, MAX(score) AS sscore FROM Submissions AS s GROUP BY s.hacker_id, s.challenge_id) st ON h.hacker_id = st.hacker_id
+SELECT h.hacker_id, h.name, SUM(sscore) 
+FROM Hackers AS h JOIN (SELECT hacker_id, MAX(score) AS sscore FROM Submissions GROUP BY hacker_id, challenge_id) res ON res.hacker_id = h.hacker_id
 GROUP BY h.hacker_id, h.name
 HAVING SUM(sscore) > 0
-ORDER BY SUM(sscore) DESC, hacker_id ASC;
+ORDER BY SUM(sscore) DESC, h.hacker_id ASC;
